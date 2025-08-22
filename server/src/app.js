b0ffe897942f39b88b07661fc28909d079f828f7
Sarthak <nucleosarthak279@gmail.com>
2025-08-23 00:41:28 +0530

    cors updated

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ const upload = require("./middlewares/multer.middleware")
 
 // CORS middleware - IMPORTANT for frontend-backend communication
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*') // We'll update this after frontend deployment
+    res.header('Access-Control-Allow-Origin', 'https://vali-doc.vercel.app/') 
     res.header('Access-Control-Allow-Credentials', 'true')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
